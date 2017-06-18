c580ceb33450140e643d84c753505bb3d128eb78
jvpupcat <vuong.joann@yahoo.com>
2017-06-18 18:47:30 +0000

    first commit

---
+++ printf_header.h
@@ -0,0 +1,11 @@
+#ifndef PRINTF_HEADER_H
+#define PRINTF_HEADER_H
+
+/* HELPER FUNCTIONS */
+int _putchar(char c);
+int _strlen(char *str);
+int _strcmp(char *s1, char *s2);
+
+/* MAIN FUNCTIONS */
+
+#endif
