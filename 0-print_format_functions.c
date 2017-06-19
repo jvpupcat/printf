91d2265d0c88d7a073c85f8311e31ee5e10c7f1e
scoyne84 <scoyne84@yahoo.com>
2017-06-19 21:06:05 +0000

    updated return values

--- 0-print_format_functions.c
+++ 0-print_format_functions.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
 /**
- * _print_string - 
- * @str:
+ * _print_string - prints a string
+ * @str: string being passed to function for print
  * Return: void.
  */
-void _print_string(char *str)
+int _print_string(char *str)
 {
 	int x = 0;
 
@@ -14,17 +14,19 @@ void _print_string(char *str)
 		x++;
 	}
 	_putchar('\n');
+	return (0);
 }
 /**
- * _print_char - 
- * @c:
+ * _print_char - prints a character
+ * @c: character being passed to function
  * Return: void.
  */
-void _print_char(char *c)
+int _print_char(char *c)
 {
 	if (*c != '\0')
 	{
 		_putchar(*c);
 	}
 	_putchar('\n');
+	return (0);
 }
