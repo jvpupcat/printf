95e1456d7cfb4596929fbdaec08ba95f4500df24
jvpupcat <vuong.joann@yahoo.com>
2017-06-20 09:24:49 +0000

    added itoa

--- 0-print_format_functions.c
+++ 0-print_format_functions.c
@@ -1,4 +1,18 @@
 #include "holberton.h"
+
+/**
+ * _print_mod - prints % if found twice consecutively
+ * @str: string being passed through function
+ * Return: always 0.
+ */
+int _print_mod(va_list list)
+{
+	va_arg(list, int);
+
+	_putchar('%');
+	return (0);
+}
+
 /**
  * _print_string - prints a string
  * @str: string being passed to function for print
@@ -14,8 +28,9 @@ int _print_string(va_list list)
 		_putchar(str[x]);
 		x++;
 	}
-	return (0);
+	return (x);
 }
+
 /**
  * _print_char - prints a character
  * @str: character being passed to function
@@ -26,17 +41,29 @@ int _print_char(va_list list)
 	char str =(char)va_arg(list, int);
 
 	_putchar(str);
-	return (0);
+	return (str);
 }
-/**
- * _print_mod - prints % if found twice consecutively
- * @str: string being passed through function
- * Return: always 0.
- */
-int _print_mod(va_list list)
-{
-	va_arg(list, int);
 
-	_putchar('%');
-	return (0);
+/**
+ * _print_d_i - function that converts string to integer
+ * @list: va_list list
+ * Return: 0 upon success
+ **/
+int _print_d_i(va_list list)
+{
+	char str = va_arg(list, int);
+	int x;
+
+	if (str < 0)
+	{
+		str = -str;
+	}
+	do 
+	{
+		str[x++] = n % 10 + '0';
+	} while ((n /= 10) > 0);
+	if (str < 0)
+		str[x++] = '-';
+	str[x] = '\0';
+	return (x);
 }
