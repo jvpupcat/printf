e4f1b9c36641a2fd925c32a7802920363c19f7ea
jvpupcat <vuong.joann@yahoo.com>
2017-06-20 16:02:46 +0000

    added counter

--- 0-print_format_functions.c
+++ 0-print_format_functions.c
@@ -3,44 +3,50 @@
 /**
  * _print_mod - prints % if found twice consecutively
  * @str: string being passed through function
- * Return: always 0.
+ * Return: counter
  */
 int _print_mod(va_list list)
 {
+	int counter = 0;
 	va_arg(list, int);
 
 	_putchar('%');
-	return (0);
+	counter += 1;
+	return (counter);
 }
 
 /**
  * _print_string - prints a string
  * @str: string being passed to function for print
- * Return: void.
+ * Return: counter
  */
 int _print_string(va_list list)
 {
+	int counter = 0;
 	char *str = va_arg(list, char *);
 	int x = 0;
 
 	while (str[x])
 	{
 		_putchar(str[x]);
+		counter += 1;
 		x++;
 	}
-	return (x);
+	return (counter);
 }
 
 /**
  * _print_char - prints a character
  * @str: character being passed to function
- * Return: void.
+ * Return: counter
  */
 int _print_char(va_list list)
 {
+	int counter = 0;
 	char str =(char)va_arg(list, int);
 
 	_putchar(str);
+	counter += 1;
 	return (str);
 }
 
@@ -48,7 +54,7 @@ int _print_char(va_list list)
  * _print_d_i - function that converts string to integer
  * @list: va_list list
  * Return: 0 upon success
- **/
+
 int _print_d_i(va_list list)
 {
 	char str = va_arg(list, int);
@@ -66,4 +72,4 @@ int _print_d_i(va_list list)
 		str[x++] = '-';
 	str[x] = '\0';
 	return (x);
-}
+}**/
