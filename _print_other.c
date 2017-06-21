c390eddf8681a5e73e9bc2c007f0a68872a51e5b
jvpupcat <vuong.joann@yahoo.com>
2017-06-21 23:42:34 +0000

    removed \n

--- _print_other.c
+++ _print_other.c
@@ -14,11 +14,36 @@ int _print_rev(va_list list)
 		str = ")llun(";
 	for (x = 0; str[x] != '\0'; x++)
 		;
-	for (x--; x >= 0; x--)
+	for (x -= 1; x >= 0; x--)
 	{
 		_putchar(str[x]);
 		counter += 1;
 	}
-	_putchar('\n');
 	return (counter);
+}
+
+/**
+ * rot13 - function that encodes a string using rot13
+ * @x: pointer
+ * Return: 0 upon success
+ **/
+char *rot13(char *x)
+{
+	int a;
+	int b;
+	char s[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char t[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (a = 0; x[a] != '\0'; a++)
+	{
+		for (b = 0; s[b] != '\0'; b++)
+		{
+			if (s[b] == x[a])
+			{
+				x[a] = t[b];
+				break;
+			}
+		}
+	}
+	return (x);
 }
